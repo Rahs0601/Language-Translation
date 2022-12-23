6fcb90ea1ab3164187d798b45dc8e437773e2892
rahs0601 <rahulshettigar01@gmail.com>
2022-12-23 10:41:44 +0530

    new

--- app.py
+++ app.py
@@ -1,77 +1,112 @@
 import streamlit as st
 import speech_recognition as sr
+from playsound import playsound
+import os
+import pandas as pd
+
 r = sr.Recognizer()
 
+
 def main():
-    st.title('Speech to Speech Translation Using Machine Learning')
-    st.subheader('This is a demo of a speech to speech translation app using machine learning')
+    st.title("Speech to Speech Translation Using Machine Learning")
+    st.subheader(
+        "This is a demo of a speech to speech translation app using machine learning"
+    )
+
 
 def Voice():
-    st.title('Recognizing Speech')
+    st.title("Recognizing Speech")
     with sr.Microphone() as source:
-        st.write("Say something!")
+        st.write("Speak Anything :")
         audio = r.listen(source)
-        st.write("Time over, thanks")
+        st.write("The Audio is Recorded")
     return audio
+
 
 def kannadaAudioCreater(Text):
     from gtts import gTTS
-    import os
-    language = 'kn'
+
+    language = "kn"
     myobj = gTTS(text=Text, lang=language, slow=False)
     myobj.save("Kannada.mp3")
-    os.system("Kannada.mp3")
-    return 
+    # play mp;3 using os
+    # os.system("Kannada.mp3")
+    audio_file = open("Kannada.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.audio(audio_bytes, format="audio/mp3")
+    return
+
 
 def EnglishSpeaker(Text):
     import pyttsx3
+
     engine = pyttsx3.init()
     engine.say(Text)
     engine.runAndWait()
     return
 
-#kannada Speech to English Speech
- 
+
+# kannada Speech to English Speech
+
+
 def KannadaInput(audio):
     try:
-        InputText = r.recognize_google(audio,language='kn-IN')
-        st.write("You said " + InputText )
-    except :
+        InputText = r.recognize_google(audio, language="kn-IN")
+        st.write("You said " + " ' " + InputText + " ' ")
+    except:
         st.write("Your Audio Wasn't Clear Plz Try again")
     return InputText
 
 
 def EnglishOutput(InputText):
     from deep_translator import GoogleTranslator
-    outputText = GoogleTranslator(source='kn', target='en').translate(InputText)
+
+    outputText = GoogleTranslator(source="kn", target="en").translate(InputText)
     return outputText
 
-#English Speech to Kannada Speech
+
+# English Speech to Kannada Speech
+
+
 def EnglishInput(audio):
     try:
-        InputText = r.recognize_google(audio,language='en-IN')
-        st.write("You said " + InputText )
-    except :
+        InputText = r.recognize_google(audio, language="en-IN")
+        st.write("You said " + " ' " + InputText + " ' ")
+    except:
         st.write("Your Audio Wasn't Clear Plz Try again")
     return InputText
 
+
 def KannadaOutput(InputText):
     from deep_translator import GoogleTranslator
-    outputText = GoogleTranslator(source='en', target='kn').translate(InputText)
+
+    outputText = GoogleTranslator(source="en", target="kn").translate(InputText)
     return outputText
 
-if __name__ == '__main__':
+
+def Microsoft(txt):
+    from deep_translator import MicrosoftTranslator
+
+    outputText = MicrosoftTranslator(source="kn", target="en").translate(txt)
+    return outputText
+
+
+if __name__ == "__main__":
     main()
-    if st.button('Kannada Input'):
+    if st.button("Show Data Set"):
+        data = pd.read_csv("eng-kannada.csv")
+        st.dataframe(data)
+
+    if st.button("Kannada Input"):
         audio = Voice()
-        kannada_input =KannadaInput(audio)
+        kannada_input = KannadaInput(audio)
         st.write(kannada_input)
         # kannadaAudioCreater(kannada_input)
         english_output = EnglishOutput(kannada_input)
         st.write(english_output)
         EnglishSpeaker(english_output)
 
-    if st.button('English Input'):
+    if st.button("English Input"):
         audio = Voice()
         english_input = EnglishInput(audio)
         st.write(english_input)
@@ -79,5 +114,3 @@ if __name__ == '__main__':
         kannada_output = KannadaOutput(english_input)
         st.write(kannada_output)
         kannadaAudioCreater(kannada_output)
-    
-
--- test.py
+++ test.py
@@ -1,25 +1,53 @@
-def spiralTraverse(array):
-    result = []
-    startrow, endrow = 0, len(array) - 1
-    startCol, endCol = 0, len(array[0]) - 1
-    while startrow <= endrow and startCol <= endCol:
-        for col in range(startCol, endCol + 1):
-            result.append(array[startrow][col])
-        for row in range(startrow + 1, endrow + 1):
-            result.append(array[row][endCol])
-        for col in reversed(range(startCol, endCol)):
-            if startrow == endrow:
-                break
-            result.append(array[endrow][col])
-        for row in reversed(range(startrow + 1, endrow)):
-            if startCol == endCol:
-                break
-            result.append(array[row][startCol])
-        startrow += 1
-        endrow -= 1
-        startCol += 1
-        endCol -= 1
-    return result
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
-arr = [[12,16,20,24],[28,32,36,40],[44,48,52,56],[60,64,68,72]]
-print(spiralTraverse(arr))
+# Load the translation data
+df = pd.read_csv("eng-kannada.csv")
+
+# Split the data into features (X) and labels (y)
+# X = df[["English"]]
+# y = df["Kannada"]
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Create a CountVectorizer object
+vectorizer = CountVectorizer()
+
+# Fit the vectorizer on the English and Kannada sentences
+vectorizer.fit(df["English"] + df["Kannada"])
+
+# Transform the English and Kannada sentences into numerical vectors
+X = vectorizer.transform(df["English"])
+y = vectorizer.transform(df["Kannada"])
+# Convert the sparse matrices to dense arrays
+X = X.toarray()
+y = y.toarray()
+
+
+# Split the data into a training set and a test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Train a random forest classifier on the training data
+clf = RandomForestClassifier()
+clf.fit(X_train[1000], y_train[1000])
+# print(X_train)
+# Make predictions on the test data
+predictions = clf.predict(X_test[1000])
+
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy:", accuracy)
+
+# Translate a sentence from English to Kannada
+english_sentence = "Hello, how are you?"
+kannada_translation = clf.predict([[english_sentence]])[0]
+print("English:", english_sentence)
+print("Kannada:", kannada_translation)
+
+# Translate a sentence from Kannada to English
+kannada_sentence = "ನಮಸ್ಕಾರ, ನೀವು ಹೇಗೆ ಹೋಗುತ್ತೀರಿ?"
+english_translation = clf.predict([[kannada_sentence]])[0]
+print("Kannada:", kannada_sentence)
+print("English:", english_translation)
